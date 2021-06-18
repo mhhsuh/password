2cfbeccbc54838f274cbc1c7ed1fd0f48c8e7da7
MH <mhhsu6110@gmail.com>
2021-06-18 17:13:50 +0800

    Update while x > 0: condition

--- password.py
+++ password.py
@@ -1,13 +1,18 @@
 password = "a123"
 x = 3
-while x > 0:		
+while x > 0:	
+	x = x - 1	
 	key_in = input('please key in your password: ')		
 	if key_in == password:
 		print('login success')
 		break
 	else:
-		print('login fail, you have', x - 1, ' more time to try')
-		x = x - 1
+		print('login fail')
+		if x > 0:
+			print('you have', x , ' more time to try')
+		else:
+			print('your account is locked, no further chance')
+
 
 
 
