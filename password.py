8c461e9cf61153a5324ead501fe2bf3a816a3f6a
MH <mhhsu6110@gmail.com>
2021-06-18 16:58:17 +0800

    update password file

--- password.py
+++ password.py
@@ -1,17 +1,13 @@
 password = "a123"
-x = 0
-x = int(x)
-while True:	
-	if x < 3:
-		key_in = input('please key in your password: ')		
-		if key_in == 'a123':
-			print('login success')
-			break
-		else:
-			y = 2 - x
-			print('login fail, you have', y, ' more time to try')
-			x = x + 1
-	else:
+x = 3
+while x > 0:		
+	key_in = input('please key in your password: ')		
+	if key_in == password:
+		print('login success')
 		break
+	else:
+		print('login fail, you have', x - 1, ' more time to try')
+		x = x - 1
+
 
 
